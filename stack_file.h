aa947be49947676cbd89685d5f5a92224f9fd406
NullVoidNan <128657358+QuixoticLeprechauns@users.noreply.github.com>
2023-04-04 14:37:46 -0700

    Added the stack header.
    Contains the macros required to quickly and easily construct and utilize pointer based stacks.

---
+++ stack_file.h
@@ -0,0 +1,34 @@
+#ifndef FILE_STACK_H
+#define FILE_STACK_H
+
+/**
+ * Pushes value B to stack A. Returns value pushed.
+*/
+#define stack_push(A,B) (*(++(A.index)) = (B))
+
+/**
+ * Pops the value on top of stack A. Returns value popped.
+*/
+#define stack_pop(A) (*((A.index)--))
+
+/**
+ * Peeks at the value on top of stack A, without modifying it. Returns value peeked at.
+*/
+#define stack_peek(A) (*(A.index))
+
+/**
+ * Allows casting to an already declared variant of stack.
+*/
+#define stack_t(T) struct stack_##T
+
+/**
+ * Implements a structure for a new variation of stack that holds the type T. 
+*/
+#define declare_stack(T) struct stack_##T { T *elements; T *index; int size; }
+
+/**
+ * Creates an instance of a stack from a pointer to it's first element E, and the size of the total memory allocated to it S.
+*/
+#define new_stack(E, S) { (E), (E), (S), }
+
+#endif
